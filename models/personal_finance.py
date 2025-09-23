965d2ea52480ee18011bd1fa4e7e526819a403f4
Anmol Garg <anmolgarg12203@gmail.com>
2025-09-23 16:27:34 +0530

    [Add] - Added Basic Logics

--- models/personal_finance.py
+++ models/personal_finance.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class PersonalFinance(models.Model):
     _name = 'personal.finance'
@@ -20,7 +21,7 @@ class PersonalFinance(models.Model):
         ('intrest', 'Intrest'),
         ('gift', 'Gift'),
         ('other', 'Other'),
-    ], string='Category Income', required=True, tracking=True)
+    ], string='Category Income', tracking=True)
     category_expense = fields.Selection([
         ('food', 'Food'),
         ('rent', 'Rent'),
@@ -32,7 +33,7 @@ class PersonalFinance(models.Model):
         ('investment', 'Investment'),
         ('travel', 'Travel'),
         ('other', 'Other'),
-    ], string='Category Expense', required=True, tracking=True)
+    ], string='Category Expense', tracking=True)
 
     transaction_from = fields.Selection([
         ('bank_transfer', 'Bank Transfer'),
@@ -43,14 +44,31 @@ class PersonalFinance(models.Model):
     ] , string='Transaction From', required=True, tracking=True)
 
 
-    bank_ids = fields.Many2one('personal.finance.bank', string='Bank', tracking=True, required = True)
-    investment_ids = fields.Many2one('personal.finance.investment', string='Investment', tracking=True, required = True)
+    bank_ids = fields.Many2one('personal.finance.bank', string='Bank', tracking=True, required=True)
+    investment_ids = fields.Many2one('personal.finance.investment', string='Investment', tracking=True)
 
     notes = fields.Text(string='Notes', tracking=True)
     attachment = fields.Binary(string='Attachment', tracking=True)
 
     @api.model
     def create(self, vals):
-            if vals.get('transaction_name', 'New') == 'New':
-                vals['transaction_name'] = self.env['ir.sequence'].next_by_code('personal.finance') or 'New'
-            return super(PersonalFinance, self).create(vals)
+       
+        if vals.get('transaction_name', 'New') == 'New':
+            vals['transaction_name'] = self.env['ir.sequence'].next_by_code('personal.finance') or 'New'
+
+        bank_id = vals.get('bank_ids')
+        amount = vals.get('amount', 0)
+        if vals.get('transaction_type') == 'income' and bank_id and amount:
+            bank = self.env['personal.finance.bank'].browse(bank_id)
+            bank.balance += amount
+        return super(PersonalFinance, self).create(vals)
+
+    @api.constrains('amount', 'bank_ids')
+    def _check_amount_vs_bank_balance(self):
+        for record in self:
+            if record.bank_ids and record.amount > record.bank_ids.balance:
+                raise ValidationError(
+                    "Transaction amount cannot be greater than the available bank balance (%s)." % record.bank_ids.balance
+                )
+
+
